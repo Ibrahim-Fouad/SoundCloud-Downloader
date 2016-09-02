57fffa1a4fe878b2f8ef7df5f7314449824806af
Ibrahim-Fouad <iibrahim.fouad@gmail.com>
2016-09-02 23:52:00 +0200

    Add files via upload

---
+++ SoundCloud Downloader/SoundCloud Downloader/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SoundCloud_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblTrackName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTrackLink = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnGO);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.lblTrackName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTrackLink);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(526, 154);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(470, 16);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(39, 23);
+            this.btnGO.TabIndex = 6;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(132, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save & Download";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 80);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(490, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // lblTrackName
+            // 
+            this.lblTrackName.AutoSize = true;
+            this.lblTrackName.Location = new System.Drawing.Point(16, 53);
+            this.lblTrackName.Name = "lblTrackName";
+            this.lblTrackName.Size = new System.Drawing.Size(113, 13);
+            this.lblTrackName.TabIndex = 2;
+            this.lblTrackName.Text = "Track Name: unknown";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Track Link:";
+            // 
+            // txtTrackLink
+            // 
+            this.txtTrackLink.Location = new System.Drawing.Point(80, 18);
+            this.txtTrackLink.Name = "txtTrackLink";
+            this.txtTrackLink.Size = new System.Drawing.Size(384, 20);
+            this.txtTrackLink.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 181);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(551, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(255, 17);
+            this.lblStatus.Text = "-- Paste track link and click go to download it...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 203);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soundcloud Downloader - Ibrahim Fouad";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTrackLink;
+        private System.Windows.Forms.Label lblTrackName;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button btnGO;
+    }
+}
+
---
+++ SoundCloud Downloader/SoundCloud Downloader/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json.Linq;
+namespace SoundCloud_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public const string AppID = "3d9f15301a25e269942f939e2de89b98";
+        public string DownloadLink { get; set; }
+        WebClient wc;
+        public Form1()
+        {
+            InitializeComponent();
+            wc = new WebClient();
+            wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+            wc.DownloadFileCompleted += wc_DownloadFileCompleted;
+        }
+
+        void wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Track downloaded successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtTrackLink.Enabled = true;
+            button1.Enabled = true;
+            lblStatus.Text = "-- Downloading Completed!";
+            progressBar1.Value = 0;
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = string.Format("-- {0}( {2}% ) / {1} ", ConvertBytes(e.BytesReceived), ConvertBytes(e.TotalBytesToReceive), e.ProgressPercentage);
+        }
+        void GetDirectLink(string Link)
+        {
+            string ApiLink = string.Format("http://api.soundcloud.com/resolve?url={0}&client_id={1}",Link,AppID);
+            dynamic TrackInfo = JObject.Parse(wc.DownloadString(ApiLink));
+            ApiLink = string.Format("{0}?client_id={1}",TrackInfo.stream_url.ToString(),AppID);
+            DownloadLink = ApiLink;
+            this.Invoke(() =>
+            {
+                lblTrackName.Text = string.Format("Track Name: {0}", Encoding.UTF8.GetString(Encoding.UTF8.GetBytes(TrackInfo.title.ToString())));
+                lblStatus.Text = "-- Success, Click Button to start download...";
+                button1.Enabled = true;
+                btnGO.Enabled = true;
+            });
+        }
+        string ConvertBytes(long bytes)
+        {
+              string[] SizeSuffixes = 
+                   { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+              if (bytes < 0) { return "-" + ConvertBytes(-bytes); }
+              if (bytes == 0) { return "0.0 bytes"; }
+
+              int mag = (int)Math.Log(bytes, 1024);
+              decimal adjustedSize = (decimal)bytes / (1L << (mag * 10));
+              return string.Format("{0:n1} {1}", adjustedSize, SizeSuffixes[mag]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Audio Files |*.mp3";
+            sfd.Title = "Save Audio File";
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                wc.DownloadFileAsync(new Uri(DownloadLink), sfd.FileName);
+                txtTrackLink.Enabled = false;
+                button1.Enabled = false;
+                btnGO.Enabled = false;
+            }
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            btnGO.Enabled = false;
+            lblStatus.Text = "-- Getting track info...";
+            System.Threading.Thread thr = new System.Threading.Thread(() => { GetDirectLink(txtTrackLink.Text.Trim()); });
+            thr.IsBackground = true;
+            thr.Start();
+        }
+    }
+}
+
+//For Safe Thread
+public static partial class ControlExtensions
+{
+    public static void Invoke(this Control control,Action action)
+    {
+        if (control.InvokeRequired)
+        {
+            control.Invoke(new MethodInvoker(action), null);
+        }
+        else
+        {
+            control.Invoke(action);
+        }
+    }
+}
